93d82a3b0be2a0c9ee3ab5f663ba3a53fe418f49
Demian1000 <dem@ciencias.unam.mx>
2023-09-07 16:01:19 -0600

    image

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -11,16 +11,3 @@ Hola Mundo
 form=cgi.FieldStorage()
 print ("<p>user:", form["user"].value)
 print ("<p>pass:", form["pass"].value)
-
-print("""
-<style>
-    /* Center the image horizontally */
-    .center-image {
-        text-align: center;
-    }
-</style>
-
-<div class="center-image">
-    <img src="./images/gato.png" alt="Gato">
-</div>
-""")
